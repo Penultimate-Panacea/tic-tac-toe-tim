d34236ed9c7476494936783b6ef6e730afc89c69
Rian Fantozzi <rian+github@fantozzi.org>
2023-06-26 19:59:03 -0400

    starting out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use ndarray::*;
+
+pub const BOARD_SIZE: usize = 3;
+
+
+#[derive(Clone)]
+struct Board{
+    board_data: ndarray::ArrayD<char>,
+    dimension: usize,
+}
+
+impl Board {
+    fn new() -> Self {  
+        let shape = IxDyn();
+        let data = Array::<char, _>::from_elem(shape.clone(), ' ');
+
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    unimplemented!()
 }
