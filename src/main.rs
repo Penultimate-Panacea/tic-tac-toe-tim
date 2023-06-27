eb41713ed1c2625cd75d270325ac1323a3e110f3
Rian Fantozzi <rian+github@fantozzi.org>
2023-06-26 22:42:02 -0400

    First Stab

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,100 @@
-use ndarray::*;
+const SIZE: usize = 4;
 
-pub const BOARD_SIZE: usize = 3;
-
-
-#[derive(Clone)]
-struct Board{
-    board_data: ndarray::ArrayD<char>,
-    dimension: usize,
+enum Player {
+    X,
+    O,
 }
 
-impl Board {
-    fn new() -> Self {  
-        let shape = IxDyn();
-        let data = Array::<char, _>::from_elem(shape.clone(), ' ');
+struct TicTacToe4D {
+    board: [[[[char; SIZE]; SIZE]; SIZE]; SIZE],
+    current_player: Player,
+}
 
-        
+impl TicTacToe4D {
+    fn new() -> Self {
+        TicTacToe4D {
+            board: [[[[' '; SIZE]; SIZE]; SIZE]; SIZE],
+            current_player: Player::X,
+        }
+    }
+
+    fn make_move(&mut self, x: usize, y: usize, z: usize, w: usize) -> bool {
+        if self.board[x][y][z][w] == ' ' {
+            self.board[x][y][z][w] = match self.current_player {
+                Player::X => 'X',
+                Player::O => 'O',
+            };
+            self.current_player = match self.current_player {
+                Player::X => Player::O,
+                Player::O => Player::X,
+            };
+            true
+        } else {
+            false
+        }
+    }
+
+    fn check_winner(&self) -> Option<Player> {
+        let lines_to_check: Vec<Vec<(usize, usize, usize, usize)>> = vec![
+            // Horizontal lines
+            (0..SIZE)
+                .flat_map(|x| {
+                    (0..SIZE).flat_map(move |y| {
+                        (0..SIZE).map(move |z| (x, y, z, 0)).collect::<Vec<_>>()
+                    })
+                })
+                .collect(),
+            // Vertical lines
+            (0..SIZE)
+                .flat_map(|x| {
+                    (0..SIZE).flat_map(move |z| {
+                        (0..SIZE).map(move |y| (x, y, z, 0)).collect::<Vec<_>>()
+                    })
+                })
+                .collect(),
+            // Depth lines
+            (0..SIZE)
+                .flat_map(|y| {
+                    (0..SIZE).flat_map(move |z| {
+                        (0..SIZE).map(move |x| (x, y, z, 0)).collect::<Vec<_>>()
+                    })
+                })
+                .collect(),
+            // Diagonal lines
+            (0..SIZE)
+                .flat_map(|x| {
+                    (0..SIZE).map(move |y| (x, y, y, 0)).collect::<Vec<_>>()
+                })
+                .collect(),
+            (0..SIZE)
+                .flat_map(|x| {
+                    (0..SIZE).map(move |y| (x, y, SIZE - y - 1, 0)).collect::<Vec<_>>()
+                })
+                .collect(),
+        ];
+
+        for line in lines_to_check {
+            let symbols: Vec<char> = line
+                .iter()
+                .map(|&(x, y, z, w)| self.board[x][y][z][w])
+                .collect();
+
+            if symbols.iter().all(|&s| s == 'X') {
+                return Some(Player::X);
+            } else if symbols.iter().all(|&s| s == 'O') {
+                return Some(Player::O);
+            }
+        }
+
+        None
     }
 }
 
+use std::time::Instant;
+
+// Rest of the code...
+
 fn main() {
-    unimplemented!()
+    let mut game = TicTacToe4D::new();
+
 }
